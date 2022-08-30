797e53a1ffab2bb4640e6470a11449ea5dfa84a8
Larry White <lwhite1@users.noreply.github.com>
2022-08-30 15:24:42 -0400

    Add some additional fixedWidth getters to Cursor

--- src/main/java/org/apache/arrow/table/Cursor.java
+++ src/main/java/org/apache/arrow/table/Cursor.java
@@ -1,8 +1,6 @@
 package org.apache.arrow.table;
 
-import org.apache.arrow.vector.IntVector;
-import org.apache.arrow.vector.ValueVector;
-import org.apache.arrow.vector.VarCharVector;
+import org.apache.arrow.vector.*;
 
 import java.nio.charset.Charset;
 import java.util.Iterator;
@@ -86,6 +84,386 @@ public class Cursor extends BaseCursor implements Iterator<Cursor> {
      */
     public int getInt(int columnIndex) {
         IntVector vector = (IntVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns an int from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present in the MutableCursor and an IllegalArgumentException is thrown if it
+     * has a different type
+     */
+    public int getUInt4(String columnName) {
+        UInt4Vector vector = (UInt4Vector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns an int from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present and an IllegalArgumentException is thrown if it
+     * has a different type
+     */
+    public int getUnit4(int columnIndex) {
+        UInt4Vector vector = (UInt4Vector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a short from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public short getSmallInt(String columnName) {
+        SmallIntVector vector = (SmallIntVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a short from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public short getSmallInt(int columnIndex) {
+        SmallIntVector vector = (SmallIntVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a char from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public char getUInt2(String columnName) {
+        UInt2Vector vector = (UInt2Vector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a char from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public char getUInt2(int columnIndex) {
+        UInt2Vector vector = (UInt2Vector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a byte from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public byte getTinyInt(String columnName) {
+        TinyIntVector vector = (TinyIntVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a byte from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public byte getTinyInt(int columnIndex) {
+        TinyIntVector vector = (TinyIntVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a byte from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public byte getUInt1(String columnName) {
+        UInt1Vector vector = (UInt1Vector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a byte from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public byte getUInt1(int columnIndex) {
+        UInt1Vector vector = (UInt1Vector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getBigInt(String columnName) {
+        BigIntVector vector = (BigIntVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getBigInt(int columnIndex) {
+        BigIntVector vector = (BigIntVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getUInt8(String columnName) {
+        UInt8Vector vector = (UInt8Vector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getUInt8(int columnIndex) {
+        UInt8Vector vector = (UInt8Vector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a float from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public float getFloat4(String columnName) {
+        Float4Vector vector = (Float4Vector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public float getFloat4(int columnIndex) {
+        Float4Vector vector = (Float4Vector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a double from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public double getFloat8(String columnName) {
+        Float8Vector vector = (Float8Vector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a double from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public double getFloat8(int columnIndex) {
+        Float8Vector vector = (Float8Vector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns an int from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public int getBit(String columnName) {
+        BitVector vector = (BitVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns an int from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public int getBit(int columnIndex) {
+        BitVector vector = (BitVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getTimeNano(String columnName) {
+        TimeNanoVector vector = (TimeNanoVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getTimeNano(int columnIndex) {
+        TimeNanoVector vector = (TimeNanoVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getTimeMicro(String columnName) {
+        TimeMicroVector vector = (TimeMicroVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getTimeMicro(int columnIndex) {
+        TimeMicroVector vector = (TimeMicroVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns an int from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public int getTimeMilli(String columnName) {
+        TimeMilliVector vector = (TimeMilliVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns an int from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public int getTimeMilli(int columnIndex) {
+        TimeMilliVector vector = (TimeMilliVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns an int from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public int getTimeSec(String columnName) {
+        TimeSecVector vector = (TimeSecVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns an int from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public int getTimeSec(int columnIndex) {
+        TimeSecVector vector = (TimeSecVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getTimeStamp(String columnName) {
+        TimeStampVector vector = (TimeStampVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getTimeStamp(int columnIndex) {
+        TimeStampVector vector = (TimeStampVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getTimeStampSec(String columnName) {
+        TimeStampSecVector vector = (TimeStampSecVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getTimeStampSec(int columnIndex) {
+        TimeStampSecVector vector = (TimeStampSecVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getTimeStampSecTZ(String columnName) {
+        TimeStampSecTZVector vector = (TimeStampSecTZVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getTimeStampSecTZ(int columnIndex) {
+        TimeStampSecTZVector vector = (TimeStampSecTZVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getTimeStampNano(String columnName) {
+        TimeStampNanoVector vector = (TimeStampNanoVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getTimeStampNano(int columnIndex) {
+        TimeStampNanoVector vector = (TimeStampNanoVector) table.getVector(columnIndex);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column of the given name at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it
+     * is present but has a different type
+     */
+    public long getTimeStampNanoTZ(String columnName) {
+        TimeStampNanoTZVector vector = (TimeStampNanoTZVector) table.getVector(columnName);
+        return vector.get(rowNumber);
+    }
+
+    /**
+     * Returns a long from the column with the given index at the current row. An IllegalStateException is
+     * thrown if the column is not present, and an IllegalArgumentException is thrown if it is present but
+     * has a different type
+     */
+    public long getTimeStampNanoTZ(int columnIndex) {
+        TimeStampNanoTZVector vector = (TimeStampNanoTZVector) table.getVector(columnIndex);
         return vector.get(rowNumber);
     }
 
